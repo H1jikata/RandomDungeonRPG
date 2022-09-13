a5e4f1fdca8fd4c5bb8edab434b46a64d167829e
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-14 01:47:25 +0900

    no message

--- Assets/Sc/PlayerController.cs
+++ Assets/Sc/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _speed = 5f;
+    [SerializeField] float _upForce = 10f; 
     Vector3 _dir = default;
 
     Rigidbody _rb;
@@ -15,15 +16,8 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
-        float _h = Input.GetAxisRaw("Horizontal");
-        float _v = Input.GetAxisRaw("Vertical");
-
-        _dir = new Vector3(_h, 0, _v);
-
-        if (_dir != Vector3.zero)
-        { 
-            this.transform.forward = _dir;
-        }
+        PlayerMove();
+        PlayerFly();
     }
 
     private void FixedUpdate()
@@ -31,5 +25,26 @@ public class PlayerController : MonoBehaviour
         Vector3 velocity = _dir.normalized * _speed;
         velocity.y = _rb.velocity.y;
         _rb.velocity = velocity;
+    }
+
+    private void PlayerMove()
+    {
+        float _h = Input.GetAxisRaw("Horizontal");
+        float _v = Input.GetAxisRaw("Vertical");
+
+        _dir = new Vector3(_h, 0, _v);
+
+        if (_dir != Vector3.zero)
+        {
+            this.transform.forward = _dir;
+        }
+    }
+
+    void PlayerFly()
+    {
+        if(Input.GetButton("Jump"))
+        {
+            _rb.AddForce(new Vector3(0, _upForce, 0));
+        }
     }
 }
