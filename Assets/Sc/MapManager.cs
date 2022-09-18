e591b48dd5daa72ccc099994813fd19dac20a74c
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-18 09:57:29 +0900

    区域分割法の一部

--- Assets/Sc/MapManager.cs
+++ Assets/Sc/MapManager.cs
@@ -8,13 +8,15 @@ public class MapManager : MonoBehaviour
 
     [SerializeField] int _mapVertical = 10;
     [SerializeField] int _mapWidth = 10;
+
+    [SerializeField] GameObject _wallBrock = default;
     void Start()
     {
         for(int i = 0; i < _mapVertical; i++)
         {
-            for(int j = 0; j < _mapWidth; j++)
+            for(int j = 1; j < _mapWidth + 1; j++)
             {
-
+                Instantiate(_wallBrock, new Vector3(i , 0 , -j),Quaternion.identity);
             }
         }
     }
