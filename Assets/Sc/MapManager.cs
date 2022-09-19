7cd8cdcb48d57fda08e06107fbfcb079414c3bab
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-19 22:42:07 +0900

    試行錯誤

--- Assets/Sc/MapManager.cs
+++ Assets/Sc/MapManager.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    [SerializeField] GameObject _wallPrefub = default;
+    [SerializeField] int _sizeX = 40;
+    [SerializeField] int _sizeY = 30;
+    [SerializeField] int _xRectMin = 7; // 部屋の最小X = 4
+    [SerializeField] int _yRectMin = 7; // 部屋の最小Y = 4
+    [SerializeField] int _xLoadMax = 2;
+    [SerializeField] int _yLoadMax = 2;
+    [SerializeField] int _floorMax = 6;
+    [SerializeField] int _floorPadding = 2;
 
-    [SerializeField] int _mapVertical = 10;
-    [SerializeField] int _mapWidth = 10;
-
-    [SerializeField] GameObject _wallBrock = default;
+    public enum Type
+    {
+        _none,
+        _wall,
+        _floor,
+        _loadUp,
+        _loadDown,
+        _loadRight,
+        _loadLeft,
+        _loadUpAlone,
+        _loadLeftAlone,
+        _etc1, _etc2, _etc3, _etc4, _etc5
+    }
     void Start()
     {
-        for(int i = 0; i < _mapVertical; i++)
-        {
-            for(int j = 1; j < _mapWidth + 1; j++)
-            {
-                Instantiate(_wallBrock, new Vector3(i , 0 , -j),Quaternion.identity);
-            }
-        }
+        
     }
 
     // Update is called once per frame
@@ -27,3 +37,5 @@ public class MapManager : MonoBehaviour
         
     }
 }
+
+
