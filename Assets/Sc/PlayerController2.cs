2b51b0e98f0eed48b5bd3a1ef659565cc8328474
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-21 15:07:52 +0900

    Cinemachineの追加

--- Assets/Sc/PlayerController2.cs
+++ Assets/Sc/PlayerController2.cs
@@ -4,28 +4,64 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
+    [SerializeField] bool _moveChage = true;
+    //今後QとEを使ってゲームをつくるためコントローラーを使ったゲームはつくらないと判断したため
+    //Input.GetAxisではなくGetKeyDownを使っています。
     void Update()
     {
-        //今後QとEを使ってゲームをつくるためコントローラーを使ったゲームはつくらないと判断したため
-        //Input.GetAxisではなくGetKeyDownを使っています。
-        if(Input.GetKeyDown("w"))
+        if(_moveChage == true)
         {
-            this.transform.Translate(1, 0, 0);
+            PlayerMove1();
         }
-
-        if(Input.GetKeyDown("s"))
+        else
         {
-            this.transform.Translate(-1, 0, 0);
+            PlayerMove2();
         }
+    }
 
-        if (Input.GetKeyDown("a"))
+    void PlayerMove1()
+    {
+        if (Input.GetKeyDown("w"))
         {
             this.transform.Translate(0, 0, 1);
         }
 
-        if (Input.GetKeyDown("d"))
+        if (Input.GetKeyDown("s"))
         {
             this.transform.Translate(0, 0, -1);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            this.transform.Translate(-1, 0, 0);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            this.transform.Translate(1, 0, 0);
+        }
+    }
+
+    void PlayerMove2()
+    {
+        if (Input.GetKey("w"))
+        {
+            this.transform.Translate(0, 0, 1);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            this.transform.Translate(0, 0, -1);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            this.transform.Translate(-1, 0, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            this.transform.Translate(1, 0, 0);
         }
     }
 }
