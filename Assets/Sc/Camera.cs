0e691747943f4e4cb6e29b892744309d50f00e1a
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-22 05:25:46 +0900

    ランダムエンカウントの作成

---
+++ Assets/Sc/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    [System.Obsolete]
+    void Start()
+    {
+        player = gameObject.transform.FindChild("Player(Clone)").gameObject;
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, 6.0f * Time.deltaTime);
+    }
+}
---
+++ Assets/Sc/Encounter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encounter : MonoBehaviour
+{
+    [SerializeField] int _encountNum = default;
+    [SerializeField] int _randNum = default;
+
+    [SerializeField] int _randomNumMin = 0;
+    [SerializeField] int _randonNumMax = 10;
+
+    [SerializeField] float _currentPlayerSpeed = 0;
+    Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _currentPlayerSpeed = _rb.velocity.magnitude;
+
+        _encountNum = Random.Range(_randomNumMin, _randonNumMax);
+    }
+    void Update()
+    {
+        _randNum = Random.Range(_randomNumMin, _randonNumMax);
+        Encount();
+
+        //Debug.Log(_randNum);
+    }
+
+    void Encount()
+    {
+        if (Input.GetKeyDown("w") &&  _randNum == _encountNum)
+        {
+            Debug.Log("encount");
+            Debug.Log(_randNum);
+            Debug.Log(_encountNum);
+        }
+
+        if (Input.GetKeyDown("s") &&  _randNum == _encountNum)
+        {
+            Debug.Log("encount");
+            Debug.Log(_randNum);
+            Debug.Log(_encountNum);
+        }
+
+        if (Input.GetKeyDown("a") &&  _randNum == _encountNum)
+        {
+            Debug.Log("encount");
+            Debug.Log(_randNum);
+            Debug.Log(_encountNum);
+        }
+
+        if (Input.GetKeyDown("d") &&  _randNum == _encountNum)
+        {
+            Debug.Log("encount");
+            Debug.Log(_randNum);
+            Debug.Log(_encountNum);
+        }
+    }
+}
