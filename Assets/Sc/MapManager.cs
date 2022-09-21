e9ed32dd8b5597dbd095d5377c7200e24df9c01e
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-21 14:55:31 +0900

    Playerの移動方法を変えた

--- Assets/Sc/MapManager.cs
+++ Assets/Sc/MapManager.cs
@@ -4,31 +4,23 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    [SerializeField] int _sizeX = 40;
-    [SerializeField] int _sizeY = 30;
-    [SerializeField] int _xRectMin = 7; // 部屋の最小X = 4
-    [SerializeField] int _yRectMin = 7; // 部屋の最小Y = 4
-    [SerializeField] int _xLoadMax = 2;
-    [SerializeField] int _yLoadMax = 2;
-    [SerializeField] int _floorMax = 6;
-    [SerializeField] int _floorPadding = 2;
+    [SerializeField] int _sizeX = 10;
+    [SerializeField] int _sizeY = 10;
 
-    public enum Type
-    {
-        _none,
-        _wall,
-        _floor,
-        _loadUp,
-        _loadDown,
-        _loadRight,
-        _loadLeft,
-        _loadUpAlone,
-        _loadLeftAlone,
-        _etc1, _etc2, _etc3, _etc4, _etc5
-    }
+    [SerializeField] GameObject _wall = default;
+    [SerializeField] GameObject _playerPrefub = default;
+    [SerializeField] GameObject _playerSpawn = default;
     void Start()
     {
-        
+        for(int i = 0; i < _sizeX; i++)
+        {
+            for(int j = 1; j < _sizeY; j++)
+            {
+                Instantiate(_wall, new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
+
+        Instantiate(_playerPrefub, _playerSpawn.transform);
     }
 
     // Update is called once per frame
---
+++ Assets/Sc/PlayerController2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    void Update()
+    {
+        //今後QとEを使ってゲームをつくるためコントローラーを使ったゲームはつくらないと判断したため
+        //Input.GetAxisではなくGetKeyDownを使っています。
+        if(Input.GetKeyDown("w"))
+        {
+            this.transform.Translate(1, 0, 0);
+        }
+
+        if(Input.GetKeyDown("s"))
+        {
+            this.transform.Translate(-1, 0, 0);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            this.transform.Translate(0, 0, 1);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            this.transform.Translate(0, 0, -1);
+        }
+    }
+}
