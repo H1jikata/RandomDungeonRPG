500737fb2cc2f89e3233b88424c3fa84aa4d00ee
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-22 07:05:22 +0900

    a版

--- Assets/Sc/Fade.cs
+++ Assets/Sc/Fade.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Fade : MonoBehaviour
 {
     [Header("FadeのPanel関係")]
     [SerializeField] float _fadeSpeed = 0.02f;
     [SerializeField] float _alfa = default;
-
     [SerializeField] bool Isfade = false;
+
+    [SerializeField, Tooltip("戦闘シーンの名前")] string _sceneName = default;
     Image _fadeImage;
 
     public static Fade instance;
@@ -34,6 +36,11 @@ public class Fade : MonoBehaviour
         {
             _alfa += _fadeSpeed;
             _fadeImage.color = new Color(0, 0, 0, _alfa);
+
+            if (_alfa >= 1)
+            {
+                SceneManager.LoadScene(_sceneName);
+            }
         }
     }
 
