ca5c468f7ea29b5d573a657e5df4d5d9e3319f51
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-09-27 09:54:51 +0900

    ResourcesファイルにTextをいれてunityで文字をとってこれるかのテスト

--- Assets/Sc/MapCreate.cs
+++ Assets/Sc/MapCreate.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapCreate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Text _text = default;
+    //[SerializeField] TextAsset _textAsset = default;
+    [SerializeField] string _loadText = default;
+    [SerializeField] int[] _splitText = default;
+
+    [SerializeField] string _textFileName = default;
     void Start()
     {
-        
+        _loadText = (Resources.Load(_textFileName, typeof(TextAsset)) as TextAsset).text;
+        string[] line = _loadText.Split(' ');
+        for(int i = 0; i < line.Length; i++)
+        {
+            //_splitText[i] = int.Parse(line[i]);
+            Debug.Log(line[i]);
+        }
     }
 
     // Update is called once per frame
