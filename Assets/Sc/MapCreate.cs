fd14335781795a104ad4c3191e1d413662453f06
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-10-06 20:36:00 +0900

    if文の追加

--- Assets/Sc/MapCreate.cs
+++ Assets/Sc/MapCreate.cs
@@ -8,9 +8,11 @@ public class MapCreate : MonoBehaviour
     [SerializeField] Text _text = default;
     //[SerializeField] TextAsset _textAsset = default;
     [SerializeField] string _loadText = default;
-    [SerializeField] int[] _splitText = default;
+    [SerializeField] string[] _splitText = default;
 
     [SerializeField] string _textFileName = default;
+
+    [SerializeField] string _wallNumber = default;
     void Start()
     {
         _loadText = (Resources.Load(_textFileName, typeof(TextAsset)) as TextAsset).text;
@@ -23,7 +25,10 @@ public class MapCreate : MonoBehaviour
 
         for(int j = 0; j < _splitText.Length; j++)
         {
+            if(_splitText[j] == _wallNumber)
+            {
 
+            }
         }
     }
 
