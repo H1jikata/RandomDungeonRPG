37904fb1a9f92cbcd238055612d02c70bc7605f5
ひじかた <yu.love5416@gmail.com>
2022-12-10 01:01:09 +0900

    ScriptableObjectの作成

---
+++ Assets/Sc/EnemyData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObeject/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [SerializeField, Header("名前")]
+    string _name = default;
+    [SerializeField, Header("レベル")]
+    int _exp = default;
+    [SerializeField, Header("HP")]
+    int _hp = default;
+    [SerializeField, Header("MP")]
+    int _mp = default;
+    [SerializeField, Header("攻撃力")]
+    int _str = default;
+    [SerializeField, Header("防御力")]
+    int _def = default;
+}
