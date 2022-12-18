ef112124fba5067e93a97a6d939544921b49a7d7
ひじかた <yu.love5416@gmail.com>
2022-12-18 18:18:51 +0900

    マップをcsvで読み込めるようにした

--- Assets/Sc/Map/MapManager.cs
+++ Assets/Sc/Map/MapManager.cs
@@ -5,46 +5,33 @@ using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
-    [SerializeField] Text _text = default;
-    //[SerializeField] TextAsset _textAsset = default;
-    [SerializeField] string _loadText = default;
-    [SerializeField] string[] _splitText = default;
+    [SerializeField] 
+    string _mapText = default;
+    [SerializeField] 
+    string[] _splitText = default;
+    [SerializeField]
+    int _x, _y;
+    [SerializeField] 
+    string _textFileName = default;
 
-    [SerializeField] string _textFileName = default;
-
-    [SerializeField] string _wallNumber = default;
-    [SerializeField] string _roadNumber = default;
-    [SerializeField] string _danDanNumber = default;
+    [SerializeField] 
+    GameObject _wallPrefub = default;
+    [SerializeField] 
+    GameObject _roadPrefub = default;
     void Start()
     {
-        _loadText = (Resources.Load(_textFileName, typeof(TextAsset)) as TextAsset).text;
-        string[] line = _loadText.Split(' ');
-        for (int i = 0; i < line.Length; i++)
+        _mapText = (Resources.Load(_textFileName, typeof(TextAsset)) as TextAsset).text;
+        _splitText = _mapText.Split('\n');
+
+        //csvファイルで読み込むため原則縦、横は必ず同じ数値だが念のため
+        _x = _splitText[0].Split(',').Length;
+        _y = _splitText.Length;
+
+        string[,] _map = new string[_x, _y];
+
+        for(int i = 0; i < _y; i++)
         {
-            //_splitText[i] = int.Parse(line[i]);
-            //Debug.Log(line[i]);
+
         }
-
-        for (int j = 0; j < _splitText.Length; j++)
-        {
-            if (_splitText[j] == _wallNumber)
-            {
-
-            }
-            else if (_splitText[j] == _roadNumber)
-            {
-
-            }
-            else if (_splitText[1] == _roadNumber)
-            {
-
-            }
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
