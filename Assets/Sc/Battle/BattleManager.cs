5e3c566c291c3174022d7af7bd75b3e951adb21f
土方悠冴 <yu.love5416@gmail.com>
2023-02-03 10:59:30 +0900

    大雑把なターン進行を可能に

--- Assets/Sc/Battle/BattleManager.cs
+++ Assets/Sc/Battle/BattleManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>現在のターン状態を保存する</summary>
+    BattalePhase _turnPhase = BattalePhase.SelectPhase;
+
+    private void Start()
     {
-        
+        _turnPhase = BattalePhase.SelectPhase;
+        PhaseChange();
+    }
+    void PhaseChange()
+    {
+        switch (_turnPhase)
+        {
+            case BattalePhase.SelectPhase:
+                Debug.Log("コマンドを選択してください");
+                break;
+                    
+            case BattalePhase.BattlePhase:
+                Debug.Log("ターン進行中です");
+                break ;
+
+            case BattalePhase.ResulePhaze:
+                Debug.Log("敵を倒しました");
+                break;
+        }
+    }
+    public void Battale()
+    {
+        _turnPhase = BattalePhase.BattlePhase;
+        PhaseChange();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Result()
     {
-        
+        _turnPhase = BattalePhase.ResulePhaze;
+        PhaseChange();
+    }
+    public enum BattalePhase
+    {
+        SelectPhase,
+        BattlePhase,
+        ResulePhaze
     }
 }
